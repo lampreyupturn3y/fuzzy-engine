1ab8aadeb02bceab0b607f0d712dd614b88f81be
Kventin <lamprey_upturn3y@icloud.com>
2025-05-05 22:36:13 +0200

    Create is_prime.cpp

---
+++ is_prime.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i*i <= n; i++)
+        if (n % i == 0) return false;
+    return true;
+}
+
+int main() {
+    int num;
+    cout << "Enter number: ";
+    cin >> num;
+    cout << (isPrime(num) ? "✅ Prime" : "❌ Not prime") << endl;
+}
